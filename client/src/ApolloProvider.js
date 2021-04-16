a2900d998ecc4a1c1600b4d6e4c25e9c85b9eb95
tony <effrenanthony2704@gmail.com>
2021-04-16 00:00:42 -0500

    setting apollo client, semantic ui and routes

---
+++ client/src/ApolloProvider.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import App from './App'
+import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client'
+import { ApolloProvider } from '@apollo/client/react';
+
+
+const httpLink = createHttpLink({
+  uri: 'http://localhost:4000'
+})
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache()
+})
+
+export default (
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>
+)
+
